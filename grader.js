b22708acf9ef03317f66c23fef6920a83be28162
Ubuntu <ubuntu@ip-10-251-64-80.us-west-2.compute.internal>
2013-07-23 06:22:52 +0000

    Making page pretty (HW4)

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,16 +37,16 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlFile = function(html) {
+    return cheerio.load(html);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(html, checksfile) {
+    $ = cheerioHtmlFile(html);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,14 +62,31 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var doIt = function(html, checksfile) {
+    var checkJson = checkHtmlFile(html, checksfile);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'URL of index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var htmlfile;
+    if (program.url) {
+      rest.get(program.url).on('complete', function(result) {
+        if (result instanceof Error) {
+          console.log(result.message);
+        } else {
+          doIt(result, program.checks);
+        }
+      });
+    } else {
+      html = fs.readFileSync(program.file);
+      doIt(html, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
